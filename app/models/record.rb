49f4a2938c870aba50d807d8e051429a02f079fe
Steven Bedrick <steve@bedrick.org>
2009-07-13 13:24:43 -0700

    Changed image_local_path to point to non-passworded location on skynet.

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,7 +1,8 @@
 class Record < ActiveRecord::Base
   
     def image_full_path
-        "/images/images/"+self.image_local_name
+#        "/images/images/"+self.image_local_name
+      "http://skynet.ohsu.edu/~bedrick/ic09_judge_images/images/" + self.image_local_name
 #   "http://ir.ohsu.edu/sniqrs2/images/images/" + self.image_local_name
  #  "C:\\Documents and Settings\\kalpathy\\Desktop\\iclef08\\public\\images\\images" + self.image_local_name
   
